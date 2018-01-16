aabbaf10e6a394194406973fc96f75df0f93fc2a
Gerson <gerson.vizcarra@ucsp.edu.pe>
2018-01-15 20:38:56 -0500

    Created function noise in shader

--- Project-4/Shaders/color_vShader.glsl
+++ Project-4/Shaders/color_vShader.glsl
@@ -1,6 +1,7 @@
 #version 120  // YJC: Comment/un-comment this line to resolve compilation errors
                  //      due to different settings of the default GLSL version
 
+
 attribute vec2 vPosition;
 //varying vec3 vColor;
 //varying out vec4 color;
@@ -8,10 +9,62 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+const int BIT_COUNT = 8;
+
+int modi(int x, int y)
+{
+    return x - y * (x / y);
+}
+
+int or(int a, int b)
+{
+    int result = 0, n = 1;
+    for(int i = 0; i < BIT_COUNT; i++)
+    {
+        if ((modi(a, 2) == 1) || (modi(b, 2) == 1))
+            result += n;
+        a = a / 2;
+        b = b / 2;
+        n = n * 2;
+        if(!(a > 0 || b > 0))
+            break;
+    }
+    return result;
+}
+
+float findnoise2(float x, float y)
+{
+	int n = int(floor(x)) + int(floor(y)) * 57;
+	n = or(n * 8192, n);
+	int nn = (n*(n*n*60493+19990303)+1376312589);
+	return 1.0 - (float(nn)/1073741824.0);
+}
+
+float interpolate1(float a,float b,float x)
+{
+	float ft=x * 3.1415927;
+	float f=(1.0-cos(ft))* 0.5;
+	return a*(1.0-f)+b*f;
+}
+
+
+float noise(float x,float y)
+{
+	float floorx = floor(x);//This is kinda a cheap way to floor a float integer.
+	float floory = floor(y);
+	float s, t, u, v;//Integer declaration
+	s = findnoise2(floorx, floory);
+	t = findnoise2(floorx + 1, floory);
+	u = findnoise2(floorx, floory + 1);//Get the surrounding pixels to calculate the transition.
+	v = findnoise2(floorx + 1, floory + 1);
+	float int1 = interpolate1(s, t, x - floorx);//Interpolate between the values.
+	float int2 = interpolate1(u, v, x - floorx);//Here we use x-floorx, to get 1st dimension. Don't mind the x-floorx thingie, it's part of the cosine formula.
+	return interpolate1(int1, int2, y - floory);//Here we use y-floory, to get the 2nd dimension.
+}
+
 void main()
 {
-    /*vec4 vPosition4 = vec4(vPosition.x, vPosition.y, vPosition.z, 1.0);
-    vec4 vColor4 = vec4(vColor.r, vColor.g, vColor.b, 1.0);*/
-    gl_Position = gl_ModelViewProjectionMatrix * vec4(vPosition, -1.0,1.0);
+    gl_Position = gl_ModelViewProjectionMatrix * vec4(vPosition.x, noise(vPosition.x/3, vPosition.y/3) / 5.0, vPosition.y,1.0);
+    //gl_Position = gl_ModelViewProjectionMatrix * vec4(vPosition.x, -1.0, vPosition.y,1.0);
     //color = vec4(1,1,1,1);
 }
