41cc8b2a397a709832002d740b631b8eeb249c02
Gerson <gerson.vizcarra@ucsp.edu.pe>
2018-01-30 20:38:06 -0500

    Changed project structure

--- Final-Project/headers/graph_edge.h
+++
@@ -1,7 +0,0 @@
-template <class G>
-class edge
-{
-public:
-    typedef typename G::Node Node;
-    Node* m_nodes[2];
-};
--- Final-Project/headers/model_line.h
+++
@@ -1,7 +0,0 @@
-template <class G>
-class edge
-{
-public:
-    typedef typename G::Vertex vertex;
-    vertex* vertices[2];
-};
